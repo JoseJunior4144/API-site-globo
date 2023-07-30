d63c6f6f1f5824001cb2737ba21dd22ac4eea1cc
GilvanPC <you@example.com>
2023-07-30 19:34:02 -0300

    Configurações iniciais de hospedagem

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+
+const JWTSecret = "dwfiodjkcxm,.xlsosmfdsw"
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 3001
+
+console.log("Hello World")
